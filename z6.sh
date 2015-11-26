792be1728118c1f37cd88181f52a18fe7cc87054
michbur <michalburdukiewicz@gmail.com>
2015-11-26 10:07:28 +0100

    added z7

--- z6.sh
+++ z6.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "Podaj nazwe pliku"
 read NAZWA
-echo "Liczba linii: $(echo $NAZWA | wc -l )"
+echo "Liczba linii: $(cat $NAZWA | wc -l )"
+echo "Liczba slow: $(cat $NAZWA | wc -w )"
+echo "Liczba znakow: $(cat $NAZWA | wc -m )"
